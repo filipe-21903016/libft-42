3d055c2267c5ebc1995527c1edf26bd02da2256f
Filipe Coutinho <filipezarcocoutinho@gmail.com>
2021-10-19 21:36:46 +0100

    stage ft_memchr func to test

--- libft.h
+++ libft.h
@@ -24,5 +24,6 @@ char    *ft_strchr(const char *s, int c);
 char    *ft_strrchr(const char *s, int c);
 void    *ft_memset(void *s, int c, size_t n);
 void    *ft_calloc(size_t nmemb, size_t size);
+void    *ft_memchr(const void *s, int c, size_t n);
 
 #endif
